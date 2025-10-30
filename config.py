72eff42ec5c0929f08f879b13c7fffa3e23d3e3b
Laxman K R <krlaxman03@gmail.com>
2025-10-30 20:12:15 +0530

    `horoscope_agent` to `ascendant`

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-DATABASE_URL = os.getenv("DATBASE_URL", "sqlite+aiosqlite:///./horoscope_ai.db")
+DATABASE_URL = os.getenv("DATBASE_URL", "sqlite+aiosqlite:///./ascendant.db")
 BASE_MODEL_ID = os.getenv("BASE_MODEL_ID")
