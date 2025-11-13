43470b0763998103ab8a1a67a92a7935c471640a
Laxman K R <krlaxman03@gmail.com>
2025-11-14 01:33:15 +0530

    Add: Introduce TypedDicts and constants for Nakshatras, Planets, and Houses in types.py

---
+++ ascendant/types.py
@@ -0,0 +1,113 @@
+from typing import Dict, List, Literal, Optional, TypedDict
+
+PLANETS = Literal[
+    "Sun",
+    "Moon",
+    "Mars",
+    "Mercury",
+    "Jupiter",
+    "Venus",
+    "Saturn",
+    "Rahu",
+    "Ketu",
+]
+
+RASHIS = Literal[
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricorn",
+    "Aquarius",
+    "Pisces",
+]
+
+LAGNA = Literal["Lagna"]
+
+HOUSES = Literal[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+RASHI_LORDS = Literal["Sun", "Moon", "Mars", "Mercury", "Jupiter", "Venus", "Saturn"]
+
+NAKSHATRAS = Literal[
+    "Ashwini",
+    "Bharani",
+    "Krittika",
+    "Rohini",
+    "Mrigashīrsha",
+    "Ardra",
+    "Punarvasu",
+    "Pushya",
+    "Āshleshā",
+    "Maghā",
+    "PūrvaPhalgunī",
+    "UttaraPhalgunī",
+    "Hasta",
+    "Chitra",
+    "Svati",
+    "Vishakha",
+    "Anuradha",
+    "Jyeshtha",
+    "Mula",
+    "PurvaAshadha",
+    "UttaraAshadha",
+    "Shravana",
+    "Dhanishta",
+    "Shatabhisha",
+    "PurvaBhādrapadā",
+    "UttaraBhādrapadā",
+    "Revati",
+]
+
+PADA = Literal[1, 2, 3, 4]
+
+ALLOWED_DIVISIONS = Literal[1, 2, 3, 4, 7, 9, 10, 12, 16, 20, 24, 27, 30, 40, 45, 60]
+
+
+class Nakshatra(TypedDict):
+    name: NAKSHATRAS
+    lord: PLANETS
+    pada: PADA
+
+
+class PlanetOrLagnaSignData(TypedDict):
+    name: RASHIS
+    lord: RASHI_LORDS
+    nakshatra: Nakshatra
+
+
+class Planet(TypedDict):
+    name: PLANETS
+    longitude: float
+    is_retrograde: bool
+    inSign: Literal["Exalted", "Own", "Own Exalted", "Debilitated", "Friend", "Neutral", "Enemy"]
+    sign: PlanetOrLagnaSignData
+
+
+class Lagna(TypedDict):
+    name: LAGNA
+    longitude: float
+    is_retrograde: Literal[False]
+    sign: PlanetOrLagnaSignData
+
+
+Planets = List[Planet]
+
+
+class House(TypedDict):
+    sign: RASHIS
+    planets: Planets
+    lagna: Optional[Lagna]
+
+
+Chart = Dict[HOUSES, House]
+
+
+class Yoga(TypedDict):
+    name: str
+    present: bool
+    details: str
