3bf10ad5e3ea2691d2bfd84f8f4c7325a0b09827
Laxman K R <krlaxman03@gmail.com>
2025-11-13 21:42:55 +0530

    Refactor: Enhance type hints in Yoga class for better type safety and clarity

--- ascendant/yoga/__init__.py
+++ ascendant/yoga/__init__.py
@@ -1,6 +1,7 @@
-from typing import Dict, Callable, List
+from typing import Dict, Callable, List, Union
 from vedicastro.VedicAstro import VedicHoroscopeData
 from ascendant.chart import Chart
+from ascendant.types import HouseNumber, LagnaName, PlanetName, RashiName
 
 YOGA_REGISTRY: Dict[str, Callable] = {}
 
@@ -17,18 +18,26 @@ class Yoga:
     def __init__(self, horoscope: VedicHoroscopeData):
         self.chart = Chart(horoscope).generate_varga_chart(1)
 
-    def get_house_of_planet(self, planet_name: str) -> int | None:
+    def get_house_of_planet(self, planet_name: Union[PlanetName, LagnaName]) -> HouseNumber:
         """Return house number where planet is located in the chart"""
-        planet_name_lower = planet_name.lower()
         for house, data in self.chart.items():
             planets = data.get("planets", {})
-            # Case-insensitive comparison
-            for planet_key in planets.keys():
-                if planet_key.lower() == planet_name_lower:
+            for planet in planets.keys():
+                if planet.lower() == planet_name.lower():
                     return int(house.split("_")[1])
         return None
 
-    def planet_in_kendra_from(self, base_house: int, target_planet: str) -> bool:
+    def get_house_of_rashi(self, rashi: RashiName) -> HouseNumber:
+        """Returns the house number of Rashi"""
+        for house, data in self.chart.items():
+            sign = data.get("sign", {})
+            if sign.lower() == rashi.lower():
+                return int(house.split("_")[1])
+        return None
+
+    def planet_in_kendra_from(
+        self, base_house: HouseNumber, target_planet: Union[PlanetName, LagnaName]
+    ):
         """Check if a planet is in Kendra (1, 4, 7, 10) from a reference house"""
         target_house = self.get_house_of_planet(target_planet)
         if not target_house:
@@ -36,7 +45,9 @@ class Yoga:
         kendra_houses = [(base_house + i - 1) % 12 + 1 for i in [1, 4, 7, 10]]
         return target_house in kendra_houses
 
-    def planets_in_relative_house(self, base_planet: str, relative_pos: int):
+    def planets_in_relative_house(
+        self, base_planet: Union[PlanetName, LagnaName], relative_pos: HouseNumber
+    ):
         """Return list of planets in the nth house from a base planet"""
         base_house = self.get_house_of_planet(base_planet)
         if not base_house:
@@ -46,15 +57,14 @@ class Yoga:
             self.chart.get(f"house_{target_house}", {}).get("planets", {}).keys()
         )
 
-    def get_lord_of_house(self, house_number: int) -> str:
+    def get_lord_of_house(self, house_number: HouseNumber):
         """Return House Lord for give house number"""
         for house, data in self.chart.items():
-            if house == f'house_{house_number}':
+            if house == f"house_{house_number}":
                 planets = data.get("planets", {})
-                for planet in planets.keys():
-                    return planet['rashi_lord']
+                for planet in planets.values():
+                    return planet["rashi_lord"]
                     break
-
 
     def compute_all(self) -> List[Dict]:
         """Compute all registered yogas"""
