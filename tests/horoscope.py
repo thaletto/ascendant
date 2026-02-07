774bf784870e5ef916da07a4f80513597b23a2e6
Laxman K R <krlaxman03@gmail.com>
2026-02-07 14:37:46 +0530

    feat: python package

--- tests/horoscope.py
+++ tests/horoscope.py
@@ -1,21 +1,25 @@
 import os
 from datetime import datetime
-from dotenv import load_dotenv
 from vedicastro.VedicAstro import VedicHoroscopeData
 
 from ascendant.utils import getHouseSystem
 
-load_dotenv(".env.test")
+# Default test values
+DEFAULT_BIRTH_DATE = "1990-01-01"
+DEFAULT_BIRTH_TIME = "12:00"
+DEFAULT_LATITUDE = "28.6139"  # New Delhi
+DEFAULT_LONGITUDE = "77.2090"
+DEFAULT_UTC = "+5:30"
 
-birth_date_str = os.getenv("NATIVE_BIRTH_DATE")
-birth_time_str = os.getenv("NATIVE_BIRTH_TIME")
+birth_date_str = os.getenv("NATIVE_BIRTH_DATE", DEFAULT_BIRTH_DATE)
+birth_time_str = os.getenv("NATIVE_BIRTH_TIME", DEFAULT_BIRTH_TIME)
 
 birth_date = datetime.strptime(f"{birth_date_str} {birth_time_str}", "%Y-%m-%d %H:%M")
 
-lat = float(os.getenv("NATIVE_LATITUDE"))
-lng = float(os.getenv("NATIVE_LONGITUDE"))
+lat = float(os.getenv("NATIVE_LATITUDE", DEFAULT_LATITUDE))
+lng = float(os.getenv("NATIVE_LONGITUDE", DEFAULT_LONGITUDE))
 
-utc = os.getenv("NATIVE_UTC", "+5:30")
+utc = os.getenv("NATIVE_UTC", DEFAULT_UTC)
 
 ayanamsa = os.getenv("AYANAMSA", default="Lahiri")
 house_system = os.getenv("HOUSE_SYSTEM", default="whole_sign")
