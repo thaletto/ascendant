52fc1a7df0c5a784824df66599827f12ae9286a6
thaletto <krlaxman03@gmail.com>
2026-07-19 23:54:45 +0530

    fix: mark yoga registry import side effect

--- ascendant/yoga/__init__.py
+++ ascendant/yoga/__init__.py
@@ -2,6 +2,6 @@ from importlib import import_module
 
 from ascendant.yoga.base import YOGA_REGISTRY, Yoga, register_yoga
 
-import_module("ascendant.yoga.registry")
+_ = import_module("ascendant.yoga.registry")
 
 __all__ = ["Yoga", "YOGA_REGISTRY", "register_yoga"]
